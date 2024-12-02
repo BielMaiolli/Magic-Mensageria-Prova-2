26ff9189d2d2d2b8e2326922007daa6b82f228d5
Gabriel Malavazi Rodrigues <145515242+GabrielMalava@users.noreply.github.com>
2024-12-01 21:29:20 -0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get('/test') // Definindo a rota /test
+  @Get('/test') 
   testEndpoint(): string {
     return 'Test endpoint is working';
   }
